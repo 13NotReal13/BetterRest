511e8818b3f2c4794adc51ea5dc38b97483fddff
Ivan Semikin <i.semikin.94@icloud.com>
2024-09-14 19:13:35 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Иван Семикин on 14/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
